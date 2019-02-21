36619179ea233cbe5fc77c8a295afc55c5ff0e19
Cecily <Sanders.Cecily@gmail.com>
2019-02-20 21:13:56 -0500

    worked on server

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 // https://blooming-shore-75254.herokuapp.com/
 
 var express = require("express");
+var app = express();
 var path = require("path");
+
+var PORT = process.env. PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
